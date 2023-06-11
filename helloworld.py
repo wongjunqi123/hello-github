153981335fc7527d81cb7bca1e505dad5cf5be82
wongjunqi123 <junqiwong0123@gmail.com>
2023-06-11 12:53:21 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,13 @@
 def hello(to):
     return f"hello{to}!"
 
+def goodbye(to):
+    return f"goodbye{to}"
+
 def main():
     user = input("Name: ")
     print(hello(user))
+    print(goodbye(user))
     
 if __name__ == '__main__':
     main()
